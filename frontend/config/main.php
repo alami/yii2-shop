a4e5ec096140f672eac481afbd88e8fa9d744e8a
alami <alami@km.ru>
2021-02-06 17:49:18 +0500

    rules in urlManager 4 site & w-w-w

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -40,6 +40,12 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                ''=> 'site/index',
+                '<_a:about|contact|signup|login>' => 'site/<_a>',
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+                '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+                '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
             ],
         ],
     ],
