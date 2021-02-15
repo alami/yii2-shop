87d7d2493aa8616390cab677d278eb1b3244c359
alami <alami@km.ru>
2021-02-15 14:14:08 +0500

    4-21 Product-Characteristic-Value

--- shop/services/manage/Shop/ProductManageService.php
+++ shop/services/manage/Shop/ProductManageService.php
@@ -50,6 +50,11 @@ class ProductManageService
             $category = $this->categories->get($otherId);
             $product->assignCategory($category->id);
         }
+
+        foreach ($form->values as $value) {
+            $product->setValue($value->id, $value->value);
+        }
+
         $this->products->save($product);
 
         return $product;
